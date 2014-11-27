373e49c8528cca3b3d326a1a64025fb5713b528a
jmaxxz <jmaxxz@users.noreply.github.com>
2014-11-27 13:47:43 -0600

    increase map size limit to 10mb

--- server/app.js
+++ server/app.js
@@ -7,10 +7,14 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var config = require('./config/environment');
 // Setup server
 var app = express();
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
+
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
